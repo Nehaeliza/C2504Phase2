6b78877126874b5089a94a86c45a6aa8b9b5ec08
Nehaeliza <nehamaya2923@gmail.com>
2024-10-02 10:52:46 +0530

    Animals

---
+++ Animals/Animals/MainPageForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animals
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private PeacockForm frmPeacock = new PeacockForm();
+        private DuckForm frmDuck = new DuckForm();
+
+        private CrowForm frmCrow = new CrowForm();
+
+        private void mnuPeacock_Click(object sender, EventArgs e)
+        {
+            frmPeacock.MdiParent = this;
+            frmPeacock.Show(); 
+            frmPeacock.Activate();
+
+
+        }
+
+        private void mnuDuck_Click(object sender, EventArgs e)
+        {
+            frmDuck.MdiParent = this;
+            frmDuck.Show();
+            frmDuck.Activate();
+        }
+
+        private void mnuCrow_Click(object sender, EventArgs e)
+        {
+            frmCrow.MdiParent = this;
+            frmCrow.Show();
+            frmCrow.Activate();
+
+        }
+    }
+}
---
+++ Animals/Animals/PeacockForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Animals
+{
+    partial class PeacockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbPeacock = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPeacock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPeacock
+            // 
+            this.pbPeacock.Image = global::Animals.Properties.Resources.Peacock;
+            this.pbPeacock.Location = new System.Drawing.Point(-4, 1);
+            this.pbPeacock.Name = "pbPeacock";
+            this.pbPeacock.Size = new System.Drawing.Size(809, 451);
+            this.pbPeacock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPeacock.TabIndex = 0;
+            this.pbPeacock.TabStop = false;
+            // 
+            // PeacockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbPeacock);
+            this.Name = "PeacockForm";
+            this.Text = "PeacockForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PeacockForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPeacock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPeacock;
+    }
+}
