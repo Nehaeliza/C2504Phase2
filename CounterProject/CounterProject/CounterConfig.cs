41b9664830fb91b2a0f1a92dcb65fc3462541114
Nehaeliza <nehamaya2923@gmail.com>
2024-10-02 13:09:35 +0530

    Add files via upload

---
+++ CounterProject/CounterProject/CounterConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CounterProject
+{
+    public static class CounterConfig
+    {
+        //forms 
+        public static Window FrmNewCounter { get; set; }
+
+
+        //view model
+        public static CounterViewMode VueModel { get; set; }
+
+        static CounterConfig()
+        {
+            VueModel = new CounterViewMode();
+            FrmNewCounter = new CounterWindow();
+
+
+        }
+    }
+}
---
+++ CounterProject/CounterProject/CounterViewMode.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CounterProject
+{
+    public class CounterViewMode : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        private Counter _newCounter = null;
+        public Counter NewCounter
+        {
+            get => _newCounter;
+            set { _newCounter = value; OnPropertyChanged(nameof(NewCounter)); }
+        }
+
+        //private Counter _newMinus = null;
+        //public Counter NewMinus
+        //{
+        //    get => _newMinus;
+        //    set { _newMinus = value; OnPropertyChanged(nameof(NewMinus)); }
+        //}
+
+        public ICommand PlusCommand { get; }
+        public ICommand MinusCommand { get; }
+
+        public CounterViewMode()
+        {
+            this.NewCounter = new Counter { Number = 0 };
+            //this.NewMinus = new Counter { Number = 0 };
+
+            PlusCommand = new RelayCommand(Plus);
+            MinusCommand = new RelayCommand(Minus);
+
+
+        }
+        //private int Count = 0;
+
+        public void Plus()
+        {
+            NewCounter.Number++;
+            OnPropertyChanged(nameof(NewCounter));
+        }
+
+        public void Minus()
+        {
+            NewCounter.Number--;
+            OnPropertyChanged(nameof(NewCounter));
+        }
+    }
+}
