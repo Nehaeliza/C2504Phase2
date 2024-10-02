e56f80c73de2fd1cce2e03c2d415b22baa4e5b8a
Nehaeliza <nehamaya2923@gmail.com>
2024-10-02 10:49:42 +0530

    01-10-2024

---
+++ FullNameProject/FullNameProject/FullNameConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FullNameProject
+{
+    static class FullNameConfig
+    {
+        public static Window FrmFullName { get; set; }
+
+        public static Window FrmEditFullName { get; set; }
+        public static PersonViewModel VueModel { get; set; }
+
+
+
+        static FullNameConfig()
+        {
+            VueModel = new PersonViewModel();
+            FrmFullName = new FullNameWindow();
+           
+            FrmEditFullName = new EditFullNameWindow();
+
+        }
+
+    }
+}
