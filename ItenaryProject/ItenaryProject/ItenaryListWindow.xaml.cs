640ca2aef6b0f69c5590561e03f769b95f08fb5a
Nehaeliza <nehamaya2923@gmail.com>
2024-10-02 11:09:32 +0530

    Add files via upload

---
+++ ItenaryProject/ItenaryProject/ItenaryListWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItenaryProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = ItenaryConfig.VueModel;
+
+        }
+
+
+        
+
+
+        
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            ItenaryConfig.FrmNewIternary.Show();
+            New_Itenary newIternaryWindow = (New_Itenary)ItenaryConfig.FrmNewIternary;
+            ItenaryConfig.VueModel.NewWindowClose = newIternaryWindow.WindowClose;
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (grdIternaries.SelectedIndex == -1)
+            {
+                var result = MessageBox.Show(messageBoxText: "Please select iternary to edit",
+                    caption: "Alert",
+                    button: MessageBoxButton.OK,
+                    icon: MessageBoxImage.Information);
+                return;
+            }
+            ItenaryConfig.FrmEditIternary.Show();
+            Edit_Itenary editIternaryWindow = (Edit_Itenary)ItenaryConfig.FrmEditIternary;
+            ItenaryConfig.VueModel.EditWindowClose = editIternaryWindow.WindowClose;
+        }
+    }
+}
---
+++ ItenaryProject/ItenaryProject/New Itenary.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ItenaryProject
+{
+    /// <summary>
+    /// Interaction logic for New_Itenary.xaml
+    /// </summary>
+    public partial class New_Itenary : Window
+    {
+        public New_Itenary()
+        {
+            InitializeComponent();
+            this.DataContext = ItenaryConfig.VueModel;
+
+        }
+        public void WindowClose()
+        {
+            this.Hide();
+        }
+        
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        
+    }
+}
