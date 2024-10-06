141708525e56ce28a9d0f9aa3b1c89e93d5c9a97
Nehaeliza <nehamaya2923@gmail.com>
2024-10-07 00:05:06 +0530

    Add files via upload
    BankingProject

---
+++ BankkingProjectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankkingProjectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("QuEST-Global")]
+[assembly: AssemblyProduct("BankkingProjectTests")]
+[assembly: AssemblyCopyright("Copyright © QuEST-Global 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("714f22ff-e2d6-42ae-9c28-76997bbd6582")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
